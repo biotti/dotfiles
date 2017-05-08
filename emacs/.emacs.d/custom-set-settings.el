7499c49e1dcf45aeefd1120696a71dbec878a84d
biotti <noreply@example.local>
2017-05-08 17:24:56 +0200

    Modifiche a custom

--- emacs/.emacs.d/custom-set-settings.el
+++ emacs/.emacs.d/custom-set-settings.el
@@ -7,10 +7,38 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("0ad9ed23b1f323e4ba36a7f0cbef6aff66128b94faa473aacd79317fbd24abda" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" default)))
+    ("6952b5d43bbd4f1c6727ff61bc9bf5677d385e101433b78ada9c3f0e3787af06" "a0dc0c1805398db495ecda1994c744ad1a91a9455f2a17b59b716f72d3585dde" "4cbec5d41c8ca9742e7c31cc13d8d4d5a18bd3a0961c18eb56d69972bbcf3071" "0ad9ed23b1f323e4ba36a7f0cbef6aff66128b94faa473aacd79317fbd24abda" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" default)))
+ '(fci-rule-color "#383838")
+ '(linum-format " %3i ")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (org-plus-contrib org py-autopep8 elpy powershell csharp-mode rainbow-delimiters projectile indent-guide flycheck gitignore-mode gitconfig-mode magit smex ido-ubiquitous esup color-theme-solarized mustang-theme monokai-theme molokai-theme material-theme jbeans-theme gruvbox-theme darktooth-theme darkokai-theme badwolf-theme aurora-theme color-theme use-package))))
+    (org-plus-contrib org py-autopep8 elpy powershell csharp-mode rainbow-delimiters projectile indent-guide flycheck gitignore-mode gitconfig-mode magit smex ido-ubiquitous esup color-theme-solarized mustang-theme monokai-theme molokai-theme material-theme jbeans-theme gruvbox-theme darktooth-theme darkokai-theme badwolf-theme aurora-theme color-theme use-package)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 
 
 ;; -----------------------------------------------------------------------------
