9bf3dbf060df65c9f9d29f1f7ab18f796e030e7a
biotti <noreply@example.local>
2018-04-12 10:19:49 +0200

    Aggiuinti esempi config a flycheck-gometalinter

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1095,6 +1095,21 @@
   :config
   (progn
     (flycheck-gometalinter-setup))
+  ;; ;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
+  ;; (setq flycheck-gometalinter-vendor t)
+  ;; ;; only show errors
+  ;; (setq flycheck-gometalinter-errors-only t)
+  ;; ;; only run fast linters
+  ;; (setq flycheck-gometalinter-fast t)
+  ;; ;; use in tests files
+  ;; (setq flycheck-gometalinter-test t)
+  ;; ;; disable linters
+  ;; (setq flycheck-gometalinter-disable-linters '("gotype" "gocyclo"))
+  ;; ;; Only enable selected linters
+  ;; (setq flycheck-gometalinter-disable-all t)
+  ;; (setq flycheck-gometalinter-enable-linters '("golint"))
+  ;; ;; Set different deadline (default: 5s)
+  ;; (setq flycheck-gometalinter-deadline "10s")
   :after
   flycheck
   go-mode
