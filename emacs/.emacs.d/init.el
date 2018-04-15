b97264b655c7e7fdff29b862e5ce188310ba56bd
biotti <noreply@example.local>
2018-04-15 17:40:05 +0200

    Cambiata indentazione?

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -566,10 +566,10 @@
   :ensure t
   :defer t
   :bind (("C-x o" . switch-window)
-           ("C-x 1" . switch-window-then-maximize)
-           ("C-x 2" . switch-window-then-split-below)
-           ("C-x 3" . switch-window-then-split-right)
-           ("C-x 0" . switch-window-then-delete))
+         ("C-x 1" . switch-window-then-maximize)
+         ("C-x 2" . switch-window-then-split-below)
+         ("C-x 3" . switch-window-then-split-right)
+         ("C-x 0" . switch-window-then-delete))
   :config
   )
 
