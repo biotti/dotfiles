2e6d351950d3535ae1913640fe1bb51c5b340455
biotti <noreply@example.local>
2020-01-20 11:14:55 +0100

    Solito aggiornamento custom-set-settings
    Dovuto a un refresh dei package installati

--- emacs/.emacs.d/custom-set-settings.el
+++ emacs/.emacs.d/custom-set-settings.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company counsel ivy swiper yasnippet-snippets winum which-key use-package undo-tree try switch-window swap-buffers sql-indent spacemacs-theme smartparens rainbow-delimiters py-autopep8 project-explorer powershell ox-reveal origami org-bullets markdown-mode magit json-mode indent-guide ibuffer-projectile htmlize golint go-snippets go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode flycheck-gometalinter fill-column-indicator elpy elisp-format diminish csv-mode csharp-mode counsel-tramp counsel-projectile company-restclient company-quickhelp company-jedi company-go benchmark-init amx))))
+    (company-restclient restclient sql-indent yasnippet-snippets winum which-key use-package undo-tree try switch-window swap-buffers spacemacs-theme smartparens rainbow-delimiters py-autopep8 project-explorer powershell ox-reveal origami org-plus-contrib org-bullets markdown-mode magit json-mode indent-guide ibuffer-projectile htmlize golint go-snippets go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode flycheck-gometalinter fill-column-indicator elpy elisp-format diminish csv-mode csharp-mode counsel-tramp counsel-projectile company-quickhelp company-jedi company-go benchmark-init amx))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
